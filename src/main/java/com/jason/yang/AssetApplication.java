dc9433e426724134aa68a29ed19e5d764d739d9c
yangjian <jiangnanxiaoding-max@users.noreply.github.com>
2026-08-21 13:27:07 +0800

    chore: initialize Java 21 Spring Boot project

---
+++ src/main/java/com/jason/yang/AssetApplication.java
@@ -0,0 +1,12 @@
+package com.jason.yang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jason/yang/AssetApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jason.yang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
